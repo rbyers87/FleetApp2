49133752738464833c52178286746185f2b016c4
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-08-21 10:25:36 -0500

    Update Settings.tsx

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -83,7 +83,7 @@ import React, { useState, useEffect } from 'react';
               role,
               full_name,
               badge_number,
-              auth_users (email)
+              users (email)
             `)
             .order('full_name');
 
